98e8a258dbbab3609bdc9d185cfaa00295d6868d
eraldosiniciof <eraldosinicio@gmail.com>
2021-11-05 04:57:19 -0300

    solucao:topico-como-fazer-secao-de-login-183992

---
+++ duvida17/login.js
@@ -0,0 +1,21 @@
+const $ = (elemento) => document.querySelector(elemento);
+
+$("#entrar").addEventListener("click", (ev) => {
+  ev.preventDefault();
+
+  const string = localStorage.getItem("usuario");
+  const usuarioCadastrado = JSON.parse(string);
+
+  const { login, senha } = usuarioCadastrado;
+
+  const dadosCorretos =
+    login === $("#login").value && senha === $("#senha").value;
+  console.log(dadosCorretos);
+
+  if (!dadosCorretos) {
+    alert("Dados inválidos!");
+    return;
+  }
+
+  window.location.href = "./logado.html";
+});
