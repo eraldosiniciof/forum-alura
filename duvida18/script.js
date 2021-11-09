aff25a39a75b4befb16e8c5d4e23f239336d7ff4
eraldosiniciof <eraldosinicio@gmail.com>
2021-11-09 06:41:15 -0300

    resolvido: topico-nao-salva-os-itens-184483

--- duvida18/script.js
+++ duvida18/script.js
@@ -1,10 +1,22 @@
-var campos[ document.querySelector("#data"), document.querySelector("#quantidade"), document.querySelector("#valor") ];
+var campos = [
+  document.querySelector("#data"),
+  document.querySelector("#quantidade"),
+  document.querySelector("#valor"),
+];
 
-console.log(campos); var tbody = document.querySelector('table tbody'); document.querySelector('.form').addEventListener('submit',function(event){ event.PreventDefault(); var tr = document.createElement('tr'); campos.forEach(function(campo){ var td = document.createElement('td'); td.textContent = campo.value; tr.appendChild(td); });
+console.log(campos);
+var tbody = document.querySelector("table tbody");
+document.querySelector(".form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  var tr = document.createElement("tr");
+  campos.forEach(function (campo) {
+    var td = document.createElement("td");
+    td.textContent = campo.value;
+    tr.appendChild(td);
+  });
 
-var tdVolume = document.createElement('td');
-tdVolume.TextContent = campos[1].value * campos[2].value;
-tr.AppendChild(tdVolume);
-tbody.AppendChild(tr);
+  var tdVolume = document.createElement("td");
+  tdVolume.textContent = campos[1].value * campos[2].value;
+  tr.appendChild(tdVolume);
+  tbody.appendChild(tr);
 });
-
